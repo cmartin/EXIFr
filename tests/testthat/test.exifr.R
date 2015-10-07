c7d30e99a98c0bb514e05c29e6876a4db6d86e77
Charles Martin <martin.charles@gmail.com>
2015-10-07 15:00:09 -0400

    This new image fails

--- tests/testthat/test.exifr.R
+++ tests/testthat/test.exifr.R
@@ -2,11 +2,21 @@ library(LAI)
 context("Basic EXIF extraction")
 
 expect_that(
-  read_exif_tags(system.file("extdata", "tiny.jpg", package = "EXIFr"))[["Make"]],
+  read_exif_tags(system.file("extdata", "preview.jpg", package = "EXIFr"))[["Make"]],
   equals("Canon")
 )
 
 expect_that(
-  read_exif_tags(system.file("extdata", "tiny.jpg", package = "EXIFr"))[["ApertureValue"]],
+  read_exif_tags(system.file("extdata", "preview.jpg", package = "EXIFr"))[["ApertureValue"]],
   equals("43/8")
 )
+
+expect_that(
+  read_exif_tags(system.file("extdata", "WSCT0151.JPG", package = "EXIFr"))[["Make"]],
+  equals("Wingscapes")
+)
+
+# expect_that(
+#   read_exif_tags(system.file("extdata", "WSCT0151.JPG", package = "EXIFr"))[["ApertureValue"]],
+#   equals("3/1")
+# )
